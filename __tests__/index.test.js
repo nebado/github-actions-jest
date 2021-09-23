084695c1bd118e6c4e7867ecf92dbece4a5dba72
Nebado <nebado.dev@gmail.com>
2021-09-23 23:07:24 +0300

    Add faulty test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,3 +3,7 @@ const multiply = require('../index').multiply;
 test('multiply 2 4', () => {
     expect(multiply(2, 4)).toBe(8);
 });
+
+test('multiply 2 4', () => {
+    expect(multiply(2, 4)).toBe(10);
+});
